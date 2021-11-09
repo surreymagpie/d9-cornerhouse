2d2984a611963891f4462fd5d326788949134c77
Rob Curtis <surreymagpie@yahoo.co.uk>
2021-11-09 10:54:28 +0000

    Set up BrowserSync for live reloading

--- web/themes/custom/cornerhouse/gulpfile.js
+++ web/themes/custom/cornerhouse/gulpfile.js
@@ -1,5 +1,6 @@
 const { src, dest, series, watch } = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
+var browserSync = require('browser-sync').create();
 let source = {
   sass: 'sass/**/*.sass',
   css: 'css',
@@ -8,10 +9,20 @@ let source = {
 function styles() {
   return src(source.sass)
     .pipe(sass({ outputStyle: "expanded" }).on('error', sass.logError))
-    .pipe(dest(source.css));
+    .pipe(dest(source.css))
+    .pipe(browserSync.stream());
 };
 
-exports.default = styles;
+
+function serve() {
+  browserSync.init({
+    proxy: "http://d9-cornerhouse.ddev.site/"
+  });
+  watch(source.sass, styles);
+};
+
+exports.default = series(styles, serve);
+exports.serve = serve;
 exports.watch = function () {
   watch(source.sass, styles);
 };
