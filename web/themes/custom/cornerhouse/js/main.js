d12dc74262da1a7e70ff2b1587f0559f97bfb1c7
Rob Curtis <surreymagpie@yahoo.co.uk>
2022-09-14 16:12:36 +0100

    Add functionality to open and close menu

--- web/themes/custom/cornerhouse/js/main.js
+++ web/themes/custom/cornerhouse/js/main.js
@@ -1,2 +1,15 @@
 // Detect if client browser has JavaScript enabled
 document.querySelector('html').classList.replace('no-js', 'js');
+
+const button = document.querySelector('#menu-toggle');
+const ul = document.querySelector('#primary-menu');
+
+button.addEventListener('click', () => {
+  if (ul.classList.contains('menu--open')) {
+    ul.classList.remove('menu--open');
+    button.setAttribute('aria-expanded', 'false');
+  } else {
+    ul.classList.add('menu--open');
+    button.setAttribute('aria-expanded', 'true');
+  }
+});
